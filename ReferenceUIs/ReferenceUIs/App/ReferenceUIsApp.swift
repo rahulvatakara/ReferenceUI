47858a262edf93a51e76d8db30300623c05aeb78
Rahul <rahulvatakara@gmail.com>
2023-06-22 16:16:14 +0100

    .gitignore is now working

---
+++ ReferenceUIs/ReferenceUIs/App/ReferenceUIsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReferenceUIsApp.swift
+//  ReferenceUIs
+//
+//  Created by Rahul Changaram on 22/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReferenceUIsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
